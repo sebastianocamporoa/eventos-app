a0e15b0b2b25f72fb7457472ae3485609c4e6cf9
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2025-06-29 01:44:06 -0500

    feat: Agregar recursos y fuentes personalizadas

---
+++ src/utils/templateEvent.js
@@ -0,0 +1,27 @@
+const templateEvent = {
+    name: "Evento no encontrado",
+    surname: "404",
+    price: "??.??",
+    desc: "El evento que intentaste buscar no se encuentra en nuestra base de datos. Puedes explorar todos los eventos disponibles en la sección de exploración. Haz clic en el botón 'Explorar' en la parte superior izquierda para volver a la lista completa. Para evitar este tipo de errores en el futuro, te recomendamos no acceder a los eventos escribiendo manualmente el nombre en la URL. Puede que el nombre no coincida exactamente con el registrado.",
+    link: '',
+    release: 'Fecha de realización no encontrada',
+    platforms: 'Ninguna',
+    genre: 'Ninguno',
+    developers: 'Ninguno',
+    publishers: 'Ninguno',
+    inCart: false,
+    selected: false,
+    isHovered: false,
+    isLiked: false,
+    rating: 0,
+    id: 404,
+    cover: "https://res.cloudinary.com/gianlucajahn/image/upload/c_scale,q_100,w_500/v1658843471/questionmark_wzexkq.png",
+    footage: [
+        "https://res.cloudinary.com/gianlucajahn/image/upload/v1658843471/questionmark_wzexkq.png",
+        "https://res.cloudinary.com/gianlucajahn/image/upload/v1658843471/questionmark_wzexkq.png",
+        "https://res.cloudinary.com/gianlucajahn/image/upload/v1658843471/questionmark_wzexkq.png",
+        "https://res.cloudinary.com/gianlucajahn/image/upload/v1658843471/questionmark_wzexkq.png"
+    ]
+};
+
+export default templateEvent;
