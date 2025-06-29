ad147bb41c3d000e018c88ada2b111b1ea1dfe42
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2025-06-29 01:45:50 -0500

    feat: Crear componentes Grid, NavBar y SearchBar

---
+++ src/Components/Grid/Grid.js
@@ -0,0 +1,67 @@
+import styles from './Grid.module.css';
+import React, { useEffect } from 'react';
+import Card from '../Card/Card';
+import AnimatedPage from '../../Containers/AnimatedPage/AnimatedPage';
+import { v4 as uuidv4 } from 'uuid';
+
+const Grid = props => {
+    const {
+        shownEvents,
+        reviewDisplay,
+        handleLike,
+        handleHoverEvent,
+        hoverState,
+        grid,
+        search,
+        searching,
+        handleSelectEvent
+    } = props;
+
+    useEffect(() => {
+        if (grid === false) {
+            if (document.getElementsByClassName('gridContainer')) {
+                let grid = document.getElementById('gridContainer')
+                grid.className = styles.noGrid
+            }
+        } else if (grid) {
+            if (document.getElementById('gridContainer').className === styles.noGrid) {
+                let grid = document.getElementById('gridContainer')
+                grid.className = styles.gridContainer
+            }
+        }
+    }, [grid])
+
+    return (
+        <>
+            <div className={styles.reviews} style={{ display: reviewDisplay ? "flex" : "none" }}>
+                <h2>There are no reviews yet!</h2>
+                <h3>You can add some, soon.</h3>
+            </div>
+            <div className={styles.gridContainer} style={{ display: reviewDisplay ? "none" : "grid" }} id="gridContainer">
+                {searching === false ? shownEvents.map((event, i) => {
+                    return <Card
+                        event={event}
+                        key={event.name}
+                        handleLike={handleLike}
+                        handleHoverEvent={handleHoverEvent}
+                        handleSelectEvent={handleSelectEvent}
+                        hoverState={hoverState}
+                    />
+                }) : shownEvents.map((event, i) => {
+                    if (event.name.toLowerCase().includes(search.toLowerCase())) {
+                        return <Card
+                            event={event}
+                            key={event.name}
+                            handleLike={handleLike}
+                            handleHoverEvent={handleHoverEvent}
+                            handleSelectEvent={handleSelectEvent}
+                            hoverState={hoverState}
+                        />
+                    }
+                })}
+            </div>
+        </>
+    );
+}
+
+export default Grid;
